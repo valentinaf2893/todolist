ecd2eea9d9221ac755412ad9f1b66fd31dbc7a4e
Valentina Flores <valentinaflores2893@gmail.com>
2022-08-01 15:25:31 -0400

    Added comment

---
+++ todolist/ToDoTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ToDoTableViewController.swift
+//  todolist
+//
+//  Valentina
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos : [ToDo] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+
+    }
+    
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+
+        if toDo.important {
+          cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+          cell.textLabel?.text = toDo.name
+        }
+
+        return cell
+    }
+
+}
---
+++ todolist/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  todolist
+//
+//  Created by Scholar on 7/29/22.
+//
+
+import UIKit
+
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
+
